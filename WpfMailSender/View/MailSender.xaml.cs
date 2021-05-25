c9465432edc0eb58783f95b6d9dde20f736f202e
Leonid <jiehuh87@yandex.ru>
2021-05-26 00:43:33 +0300

    upd

--- WpfMailSender/View/MailSender.xaml.cs
+++ WpfMailSender/View/MailSender.xaml.cs
@@ -23,87 +23,14 @@ namespace WpfMailSender
     /// </summary>
     public partial class MailSender : Window
     {
-        string fromPassword;
         public MailSender()
         {
             InitializeComponent();
-
-            fromPassword = System.IO.File.ReadAllText("C:\\Пароль.txt");
-            cbSenderSelect.ItemsSource = VariablesClass.Senders;
-            cbSenderSelect.DisplayMemberPath = "Key";
-            cbSenderSelect.SelectedValuePath = "Value";
-
-            cbServer.ItemsSource = VariablesClass.Server;
-            cbServer.DisplayMemberPath = "Key";
-            cbServer.SelectedValuePath = "Value";
-        }
-
-        //private void btnSendEmail_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (string.IsNullOrEmpty(tbBody.Text))
-        //    {
-        //        MessageBox.Show("Письмо не заполнено.");
-        //        tbBody.Focus();
-        //        return;
-        //    }
-        //    if (string.IsNullOrEmpty(tbTo.Text))
-        //    {
-        //        MessageBox.Show("Не указан получатель.");
-        //        tbTo.Focus();
-        //        return;
-        //    }
-
-        //    var email = new EmailSendServiceClass("jiehuh87@gmail.com", fromPassword);
-        //    email.FromWriter = tbFrom.Text;
-        //    email.ToWriter = tbTo.Text;
-        //    email.Subject = tbSubject.Text;
-        //    email.Body = tbBody.Text;
-        //    email.IsBodyHtml = false;
-        //    email.SendMessage();
-
-        //    var formOk = new UserMessageBox();
-        //    formOk.Title = "Письмо отправлено.";
-        //    formOk.Width = 300;
-        //    formOk.Height = 200;
-        //    formOk.lblMessage.Content = "Работа завершена.";
-        //    formOk.lblMessage.Foreground = Brushes.Black;
-        //    formOk.ShowDialog();
-        //}
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            var formAbout = new UserMessageBox();
-            formAbout.Title = "О себе";
-            formAbout.Width = 500;
-            formAbout.Height = 300;
-            formAbout.lblMessage.Content = "Добрый день.\nЯ Ронжин Леонид, 34года из С-Пб.\nПо специальности проектировщик.\n" +
-                "Изучаю C# для себя(смена профиля работы)\nи для написания различных макросов/команд необходимых для текущей работы.";
-            formAbout.lblMessage.Foreground = Brushes.Black;
-            formAbout.ShowDialog();
         }
 
         private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-
-        private void pnBtnUI_btnPreviousClick(object sender, RoutedEventArgs e)
-        {
-            tabControl.SelectedIndex--;
-            if (tabControl.SelectedIndex == 0)
-                pnBtnUI.IsHidebtnPrevious = true;
-            else
-                pnBtnUI.IsHideBtnNext = false;
-        }
-
-        private void pnBtnUI_btnNextClick(object sender, RoutedEventArgs e)
-        {
-            tabControl.SelectedIndex++;
-            if (tabControl.SelectedIndex == tabControl.Items.Count-1)
-                pnBtnUI.IsHideBtnNext = true;
-            else
-                pnBtnUI.IsHidebtnPrevious = false;
         }
     }
 }
--- WpfMailSender/ViewModel/ViewModel.cs
+++ WpfMailSender/ViewModel/ViewModel.cs
@@ -1,25 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using WpfMailSender.Model;
 
 namespace WpfMailSender.ViewModel
 {
-    class ViewModel
+    class ViewModel : INotifyPropertyChanged
     {
-        static string fromPassword = System.IO.File.ReadAllText("C:\\Пароль.txt");
+
+        string _subject = "";
+        string _body = "";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //static string fromPassword = System.IO.File.ReadAllText("C:\\Пароль.txt");
+        static string fromPassword = "root";
 
         public EmailSendServiceClass EmailSend { get; set; } = new EmailSendServiceClass("jiehuh87@gmail.com", fromPassword);
 
-        public string FromWriter { get; set; }
-        public string ToWriter { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
+        public string FromWriter { get; set; } = "jiehuh87@yandex.ru";
+        public string ToWriter { get; set; } = "jiehuh87@yandex.ru";
+        public string Subject { 
+            get => _subject;
+            set
+            {
+                _subject = value;
+                RaisePropertyChanged("Subject");
+            }
+        }
+        public string Body
+        {
+            get => _body;
+            set
+            {
+                _body = value;
+                RaisePropertyChanged("Body");
+            }
+        }
 
-        bool IsBodyHtml { get; set; }
+        private int _selectedListener;
+        public int SelectedListener
+        {
+            get { return _selectedListener; }
+            set
+            {
+                _selectedListener = value;
+                RaisePropertyChanged("SelectedListener");
+            }
+        }
+
+        bool IsBodyHtml { get; set; } = false;
+        
+        public Dictionary<string, string> Senders { get; set; } = VariablesClass.Senders;
+        public Dictionary<string, int> Server { get; set; } = VariablesClass.Server;
 
         public ICommand SendEmail
         {
@@ -27,12 +75,57 @@ namespace WpfMailSender.ViewModel
             {
                 return new DelegateCommand((p) =>
                 {
+                    Debug.WriteLine("Test");
                     EmailSend.FromWriter = FromWriter;
                     EmailSend.ToWriter = ToWriter;
-                    EmailSend.Subject = Subject;
-                    EmailSend.Body = Body;
-                    EmailSend.IsBodyHtml = false;
+                    EmailSend.Subject = _subject;
+                    EmailSend.Body = _body;
+                    EmailSend.IsBodyHtml = IsBodyHtml;
+                    EmailSend.SendMessage();
+                }, (p) => Subject != "" && Body != "" );
+            }
+        }
+
+        public ICommand About
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    var formAbout = new UserMessageBox();
+                    formAbout.Title = "О себе";
+                    formAbout.Width = 500;
+                    formAbout.Height = 300;
+                    formAbout.lblMessage.Content = "Добрый день.\nЯ Ронжин Леонид, 34года из С-Пб.\nПо специальности проектировщик.\n" +
+                        "Изучаю C# для себя(смена профиля работы)\nи для написания различных макросов/команд необходимых для текущей работы.";
+                    formAbout.lblMessage.Foreground = Brushes.Black;
+                    formAbout.ShowDialog();
                 }, (p) => true);
+            }
+        }
+
+        public ICommand NextTab
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Click access ");
+                    _selectedListener++;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedListener"));
+                }, (p) => true);
+            }
+        }
+        public ICommand PrevTab
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Click access ");
+                    _selectedListener--;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedListener"));
+                }, (p) => _selectedListener > 0);
             }
         }
     }
